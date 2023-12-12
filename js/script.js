4f8947b4ba8c460ecfd4dad076d08c1eac381779
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2023-12-12 09:36:31 -0300

    Ajustando

--- js/script.js
+++ js/script.js
@@ -1,69 +1,5 @@
-const courses = [
-    {
-        title: 'PHP 7 Completo - Curso do Desenvolvedor Web',
-        plataform: 'Udemy',
-        emissionDate: 'Verificação emitida em out de 2023',
-        skills: 'Competências: PHP · CSS · HTML5 · MySQL · Banco de dados'
-    },
-    {
-        title: 'Treinamento Redes 5G - Softex',
-        plataform: 'Universidade Estadual do Ceará',
-        emissionDate: 'Verificação emitida em jul de 2023'
-    },
-    {
-        title: 'Banco de Dados SQL',
-        plataform: 'Microlins',
-        emissionDate: 'Verificação emitida em fev de 2022'
-    },
-    {
-        title: 'Programação C# Visual Studio (Basíco ao Avançado)',
-        plataform: 'Microlins',
-        emissionDate: 'Verificação emitida em fev de 2022'
-    },
-    {
-        title: 'Lógica de programação',
-        plataform: 'Microlins',
-        emissionDate: 'Verificação emitida em jul de 2021'
-    },
-    {
-        title: 'Pacote Office Completo - Microsoft Word, Excel e Powerpoint',
-        plataform: 'Microlins',
-        emissionDate: 'Verificação emitida em jul de 2021'
-    },
-]
-
-const projects = [
-    {
-        name: 'Pokedex/pokedex',
-        description: 'Projeto de uma pokedex em React para o projeto final de uma cadeira da faculdade',
-        repositore: 'https://github.com/MatheusFrancaDEV/pokedex',
-        tecnologies: 'React, API'
-    },
-    {
-        name: 'Eco Learning',
-        description: 'Projeto feito em conjunto para a apresentação de um trabalho',
-        repositore: 'https://github.com/MatheusFrancaDEV/eco-learning',
-        tecnologies: 'HTML, CSS e JavaScript'
-    },
-    {
-        name: 'Calculadora',
-        description: 'Calculadora simples usando a linguagem C',
-        repositore: 'https://github.com/MatheusFrancaDEV/calculadora-c',
-        tecnologies: 'C'
-    },
-    {
-        name: 'Consulta CEP',
-        description: 'Projeto de um site que consulta o CEP digitado e retorna suas informações',
-        repositore: 'https://github.com/MatheusFrancaDEV/consulta-cep',
-        tecnologies: 'HTML, CSS, JavaScript, API'
-    },
-    {
-        name: 'To-do List',
-        description: 'Projeto de um site em React de uma To-do list simples',
-        repositore: 'https://github.com/MatheusFrancaDEV/to-do-list-react',
-        tecnologies: 'React, API'
-    }
-]
+import { courses } from "./infos.js"
+import { projects } from "./infos.js"
 
 const titleCourse = document.querySelectorAll('.card-title')
 const plataformCourse = document.querySelectorAll('.card-subtitle') 
@@ -87,7 +23,7 @@ projects.forEach((item) => {
             <h3 class="project-title">${item.name}</h3>
             <hr>
             <p class="project-text">${item.description}</p>
-            <a href="${item.repositore}" class="repositore">Repositorio</a>
+            <a href="${item.repositore}" class="repositore" target="_blank">Repositorio</a>
             <hr>
             <span class="technologies fw-bold">Tecnologias: ${item.tecnologies}</span>
         </div>
