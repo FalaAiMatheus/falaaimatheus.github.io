fbd9f628a2ab78fc862336505b7de3ef66047dfa
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2024-02-20 16:27:21 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,23 +1,11 @@
-import { courses } from "../data/infos.js"
-import { projects } from "../data/infos.js"
+import { projects } from "../data/infos.js";
 
-const titleCourse = document.querySelectorAll('.card-title')
-const plataformCourse = document.querySelectorAll('.card-subtitle') 
-const emissionDateCourse = document.querySelectorAll('.card-text')
-
-courses.forEach((item, i) =>{
-    titleCourse[i].innerHTML = item.title
-    plataformCourse[i].innerHTML = item.plataform
-    emissionDateCourse[i].innerHTML = item.emissionDate
-})
-
-const projectsContainer = document.getElementById('container-project')
+const projectsContainer = document.getElementById("container-project");
 
 projects.forEach((item) => {
-
-    let newDiv = document.createElement("div");
-    newDiv.classList.add('swiper-slide')
-    newDiv.innerHTML = `
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("swiper-slide");
+  newDiv.innerHTML += `
     <div class="card">
         <div class="card-body">
             <h3 class="project-title">${item.name}</h3>
@@ -29,67 +17,65 @@ projects.forEach((item) => {
         </div>
     </div>
     `;
-    projectsContainer.appendChild(newDiv)
+  projectsContainer.appendChild(newDiv);
 });
 
-function stickyHeader(){
-    const header = document.querySelector('.header');
+function fixedHeader() {
+  const header = document.querySelector(".header");
 
-    header.classList.toggle('fixed', window.scrollY > 0);
+  header.classList.toggle("fixed", window.scrollY > 0);
 }
 
-window.addEventListener('scroll', stickyHeader)
+window.addEventListener("scroll", fixedHeader);
 
-const btnMobile = document.getElementById('btn-mobile');
+const btnMobile = document.getElementById("btn-mobile");
 
-function toggleMenu(e){
-    e.preventDefault()
-    const nav = document.querySelector('nav')
-    const links = document.querySelectorAll('a')
-    if(e.type === "touchstart") 
-    nav.classList.toggle('active');
+function toggleMenu(e) {
+  e.preventDefault();
+  const nav = document.querySelector("nav");
+  const links = document.querySelectorAll("a");
+  if (e.type === "touchstart") nav.classList.toggle("active");
 
-    links.forEach((item,i) =>{
-        links[i].addEventListener('click', () =>{
-            nav.classList.remove('active')
-        })
-    })
+  links.forEach((item, i) => {
+    links[i].addEventListener("click", () => {
+      nav.classList.remove("active");
+    });
+  });
 }
 
 btnMobile.addEventListener("click", toggleMenu);
 
 btnMobile.addEventListener("touchstart", toggleMenu);
 
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-  
-    pagination: {
-      effect: 'coverflow',
-      grabCursor: true,
-      el: '.swiper-pagination',
-      dynamicBullets: true,
-      clickableClass: '.swiper-pagination-bullet'
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  pagination: {
+    effect: "coverflow",
+    grabCursor: true,
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickableClass: ".swiper-pagination-bullet",
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-  
-    slidesPerView: 1,
-    spaceBetween: 10,
-    
-    breakpoints: {
-      
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-      
-      480: {
-        slidesPerView: 1,
-        spaceBetween: 30
-      },
-      
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      }
-    }
+
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
 });
